07c3d044e3b0f9a929bf205fab7fdbf06467d2ef
ianivo-hash <ian.ivo@edu.ufes.br>
2026-09-04 12:28:46 -0300

    Create lista15.py

---
+++ lista15.py
@@ -0,0 +1,21 @@
+x = float(input('digite a nota do trabalho'))
+y = float(input('digite a nota da avaliação'))
+z = float(input('digite a nota do exame'))
+
+m = (2*x + 3*y + 5*z)/10 
+
+print('a media e:', m) 
+
+if m >= 5: 
+  print('aluno aprovado')
+
+elif m>= 3:
+  print("aluno em recuperação")
+
+else: 
+  print('aluno reprovado")
+
+        
+  
+                
+          
