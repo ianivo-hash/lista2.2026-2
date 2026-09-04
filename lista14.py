94d8ac5712a7796ec223023a4558348f842339a4
ianivo-hash <ian.ivo@edu.ufes.br>
2026-09-04 12:20:57 -0300

    Create lista14.py

---
+++ lista14.py
@@ -0,0 +1,14 @@
+x = float(input("digite a primeira nota: "))
+y = float(input("digite a segunda nota: "))
+z = float(input("digite a terceira nota: "))
+
+m = (x + y + 2*z)/4 
+
+print("a media e:",m) 
+
+if m >= 60 :
+  print("aluno aprovado")
+
+else: 
+  print('aluno reprovado')
+  
