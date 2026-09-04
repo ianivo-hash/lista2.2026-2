40d94726dfb307753ff49fd0e87fa87c0ace48d0
ianivo-hash <ian.ivo@edu.ufes.br>
2026-09-04 12:52:33 -0300

    Create lista16.py

---
+++ lista16.py
@@ -0,0 +1,23 @@
+x = int(input('digite um numero de 1 a 7'))
+if x== 1:
+  print('domingo')
+
+elif x == 2:
+  print('segunda-feira')
+
+elif x == 3:
+  print('terca-feira')
+
+elif x == 4:
+  print('quarta feira')
+
+elif x == 5: 
+  print('quinta feira')
+
+elif x == 6: 
+  print('sexta feira')
+
+elif x== 7:
+  print('sabado')
+
+      
