eba20132d874dade0bc768024250f84957b5ad3a
ianivo-hash <ian.ivo@edu.ufes.br>
2026-09-04 13:20:25 -0300

    Create lista18.py

---
+++ lista18.py
@@ -0,0 +1,11 @@
+a = float(input('digite um numero de base maior'))
+b = float(input('digite um numero de base menor'))
+h = float(input('digite a altura'))
+
+if a > 0 and b > 0:
+  z = (a + b)*h/2
+
+  print('a area do trapezio e:, z')
+
+else: 
+  print('base invalida')
